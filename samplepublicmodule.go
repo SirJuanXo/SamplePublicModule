4620a9a7dfc1f542c60f1117085724f9ce6206db
Juan Lu Casas <casasjuanlu@gmail.com>
2025-09-29 11:37:41 +0200

    Seconde committu

---
+++ samplepublicmodule.go
@@ -0,0 +1,11 @@
+package samplepublicmodule
+
+import "fmt"
+
+func PublicFunction() {
+	fmt.Println("This is a public function in the samplepublicmodule package.")
+}
+
+func privateFunction() {
+	fmt.Println("This is a private function in the samplepublicmodule package.")
+}
